088e6e1abe30fdaacabdf565c573e26b015ba2f7
VitorHugo-br <vitor.comercialcontato@gmail.com>
2026-03-27 10:58:58 -0300

    feat: Set up initial Angular portfolio application structure, including new components like a floating menu and cards, and pages for landing, home, and projects, with Tailwind CSS integration.

--- src/app/Pages/landing-page/landing-page.ts
+++ src/app/Pages/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuFlutuante } from "../../Components/menu-flutuante/menu-flutuante";
 import { Home } from "../home/home";
+import { Projects } from "../projects/projects";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MenuFlutuante, Home],
+  imports: [MenuFlutuante, Home, Projects],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
